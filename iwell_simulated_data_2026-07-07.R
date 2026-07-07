5383086a0116b21daa55bcfd7816d8ded31e1e17
Varsha Venkatesh <venka288@umn.edu>
2026-07-07 12:14:50 -0500

    setting up project

---
+++ iwell_simulated_data_2026-07-07.R
@@ -0,0 +1,15 @@
+#######################################
+#### iWell Tutorials
+#### Data Simulation
+#### Varsha Venkatesh
+#### 2026-07-07
+#######################################
+
+# workspace setup
+library("dplyr")
+library("faux")
+library("summarytools")
+library("missMethods")
+
+# setting seed
+set.seed(2004)
